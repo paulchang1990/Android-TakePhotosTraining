8d56a7ec9fa90fc3df0ecdd398113da4c8359524
Paul Chang <paulchang1990@foxmail.com>
2015-09-09 18:06:23 +0800

    [update]:update capture part

--- src/com/paul/camgaldemo/CameraPhotoActivity.java
+++ src/com/paul/camgaldemo/CameraPhotoActivity.java
@@ -1,13 +1,10 @@
 package com.paul.camgaldemo;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,8 +15,8 @@ import android.widget.ImageView;
 import com.paul.camgaldemo.utils.CommonUtil;
 
 /**
- * 调用系统相机. 步骤如下： <li>进入系统相册Gallery</li> <li>选取需要设置的图片</li> <li>
- * 结束后返回值为图片在Gallery的provider中提供的Uri，使用resolver来解析该图片的路径等信息</li> <li>
+ * 调用系统相机. 步骤如下： <li>进入系统相机</li> <li>拍照后储存</li> <li>
+ * 结束后默认返回一个小的Bitmap对象</li> <li>
  * 根据路径解析该图片进行显示，为了防止OOM对图片进行scale处理</li>
  * 
  * @author Paul Chang
@@ -27,6 +24,8 @@ import com.paul.camgaldemo.utils.CommonUtil;
  */
 public class CameraPhotoActivity extends Activity implements OnClickListener {
 	private static final int REQUEST_CAMERA = 1;
+	private static final int REQUEST_CAMERA_SAVE_FILE = 2;
+
 	private static final boolean DEBUG = true;
 	private ImageView mImageView;
 	private String mCurrentPhotoPath;
@@ -40,87 +39,104 @@ public class CameraPhotoActivity extends Activity implements OnClickListener {
 	private void initView() {
 		setContentView(R.layout.activity_takephoto);
 		mImageView = (ImageView) findViewById(R.id.iv_image_shower);
-		findViewById(R.id.btn_open_camera).setOnClickListener(this);
+		//直接拍照
+		findViewById(R.id.btn_capture).setOnClickListener(this);
+		//拍照后存储到指定路径下
+		findViewById(R.id.btn_capture_file_specify).setOnClickListener(this);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK) {
+			//直接返回一个较小的bitmap
 			Bundle extras = data.getExtras();
 			Uri uri = data.getData();
 			Bitmap imageBitmap = null;
-			if(DEBUG){
+			if (DEBUG) {
 				System.out.println(uri);
 				imageBitmap = (Bitmap) extras.get("data");
-				System.out.println("byteCount=" + CommonUtil.getByteCount(imageBitmap));
+				System.out.println("byteCount="
+						+ CommonUtil.getByteCount(imageBitmap));
 			}
 			mImageView.setImageBitmap(imageBitmap);
+			/*
 			try {
-				imageBitmap.compress(CompressFormat.JPEG, 100, new FileOutputStream(CommonUtil.createImageFile()));
+				imageBitmap.compress(CompressFormat.PNG, 100,
+						new FileOutputStream(CommonUtil.createImageFile()));
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			*/
 		}
-		 
-//		if (requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK) {
-//			galleryAddPic();
-//		}
-	}
 
+		if (requestCode == REQUEST_CAMERA_SAVE_FILE
+				&& resultCode == Activity.RESULT_OK) {
+			CommonUtil.toast(this, "添加到图库");
+			galleryAddPic();
+		}
+	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.btn_open_camera:
+		case R.id.btn_capture:
 			goCapture();
+			break;
+		case R.id.btn_capture_file_specify:
+			goCaptureSaveSpecFile();
 			break;
 		default:
 			break;
 		}
 	}
 
-	public void galleryAddPic() {
-		//TODO mCurrentPhotoPath is null now
-	    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-	    File f = new File(mCurrentPhotoPath);
-	    Uri contentUri = Uri.fromFile(f);
-	    mediaScanIntent.setData(contentUri);
-	    this.sendBroadcast(mediaScanIntent);
+	private void goCaptureSaveSpecFile() {
+		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+			File photoFile = null;
+			try {
+				photoFile = CommonUtil.createImageFile();
+				//##start## modified by Paul Chang:google training source has a small bug,remove the prefix
+//				mCurrentPhotoPath = "file:"+photoFile.getAbsolutePath();
+				//##end##
+				mCurrentPhotoPath = photoFile.getAbsolutePath();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (photoFile != null) {
+				cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(photoFile));
+				startActivityForResult(cameraIntent, REQUEST_CAMERA_SAVE_FILE);
+			} else {
+				CommonUtil.toast(this, "文件创建失败");
+			}
+		} else {
+			CommonUtil.toast(this, "相机开启失败");
+		}
+
 	}
-	
+
+	public void galleryAddPic() {
+		Intent mediaScanIntent = new Intent(
+				Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		File f = new File(mCurrentPhotoPath);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		this.sendBroadcast(mediaScanIntent);
+	}
 
 	/**
-	 * 调用系统照相机
+	 * 直接调用系统照相机，生成的图片信息会保存到Gallery的数据库中
 	 * 
 	 * @param view
 	 */
 	private void goCapture() {
 		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(cameraIntent, REQUEST_CAMERA);
-		/**
 		if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-			System.out.println("可以开启照相机");
-			File photoFile = null;
-			try {
-				photoFile = createImageFile();
-				if(!photoFile.exists()){
-					System.out.println("创建了一个文件"+photoFile.getAbsolutePath());
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			if(photoFile != null){
-				cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-				startActivityForResult(cameraIntent, REQUEST_CAMERA);
-			}else{
-				
-				Toast.makeText(this, "文件创建失败", Toast.LENGTH_SHORT).show();
-			}
-		} else {
-			Toast.makeText(this, "无法开启相机", Toast.LENGTH_SHORT).show();
+			startActivityForResult(cameraIntent, REQUEST_CAMERA);
 		}
-		*/
+
 	}
 }
